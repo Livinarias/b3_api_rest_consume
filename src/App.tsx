0f25c648b9b172aee9ad04b84838c29c271c8c95
Livingston <livin.arias88@gmail.com>
2024-09-26 20:04:42 -0500

    feat: :package: ADD
    vite app to create coffe api carrusel

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { ApiCoffeCarruselBasic } from './components/ApiCoffeCarruselBasic'
+
+function App() {
+
+  return (
+    <div>
+      <ApiCoffeCarruselBasic/>
+    </div>
+  )
+}
+
+export default App
